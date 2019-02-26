90ddce8a80d0780f4cc0bafe5932b9af7ce8a45e
GoldySlime <39287312+GoldySlime@users.noreply.github.com>
2019-02-26 16:30:28 +0800

    Create Vtri.java

---
+++ W2/Tuesday/Vtri.java
@@ -0,0 +1,65 @@
+package Tuesday;
+
+public class Vtri {
+	private int height;
+	private int type;
+	
+	public Vtri(int h, int t) {
+		height = h;
+		type = t;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public void draw() {
+		switch(type) {
+		case 1:
+			for(int i=1; i<=height; i++) {
+				prChar("*",i);
+				System.out.println();
+			}
+			break;
+		case 2:
+			for(int i=1; i<=height; i++) {
+				prChar(" ",height-i);
+				prChar("*",i);
+				System.out.println();
+			}
+			break;
+		case 3:
+			for(int i=1; i<=height; i++) {
+				prChar("*",height-i+1);
+				System.out.println();
+			}
+			break;
+		case 4:
+			for(int i=1; i<=height; i++) {
+				prChar(" ",i-1);
+				prChar("*",height-i+1);
+				System.out.println();
+			}
+			break;
+			
+		}
+	}
+	
+	void prChar(String C, int n) {
+		for(int i=1; i<=n; i++) {
+			System.out.print(C);
+		}
+	}
+}
